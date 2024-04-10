fe7a66414f6921741075e6fe018728c31407471d
matite <matiastite1@gmail.com>
2024-04-09 23:27:04 -0500

    Implementado módulo productos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { Product } from './product.entity';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productService: ProductsService) {}
+
+  @Get()
+  async findAll(): Promise<Product[]> {
+    return this.productService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<Product> {
+    const product = await this.productService.findOne(id);
+    if (!product) {
+      throw new NotFoundException('Product does not exist!');
+    } else {
+      return product;
+    }
+  }
+
+  @Post()
+  async create(@Body() product: Product): Promise<Product> {
+    return this.productService.create(product);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() product: Product,
+  ): Promise<any> {
+    return this.productService.update(id, product);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<any> {
+    const product = await this.productService.findOne(id);
+    if (!product) {
+      throw new NotFoundException('Product does not exist!');
+    }
+    return this.productService.delete(id);
+  }
+}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [ProductsService],
 })
 export class ProductsModule {}
